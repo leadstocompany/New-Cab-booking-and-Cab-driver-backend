d9aad937221e8bdf3bf3997f4133bf79e0af4701
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-12-19 01:31:18 +0530

    refactor: update create_at to store datetime in utc in basemodel

--- utility/model.py
+++ utility/model.py
@@ -3,11 +3,7 @@ from django.utils import timezone
 
 class BaseModel(models.Model):
     is_active = models.BooleanField(default=True)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    created_at = models.DateTimeField(default=timezone.localtime(timezone.now()))
+    created_at = models.DateTimeField(default=timezone.now)  # Stores datetime in UTC
     updated_at = models.DateTimeField(auto_now=True)
     class Meta:
         abstract=True
-
-
-# timezone.localtime(timezone.now()) 

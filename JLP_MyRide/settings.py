3b3528072f5e796651614b98539826c836cd46bc
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-12-21 22:39:17 +0530

    refactor: add whitenoise to serve static file in server

--- JLP_MyRide/settings.py
+++ JLP_MyRide/settings.py
@@ -18,7 +18,10 @@ from firebase_admin import credentials
 
 env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
@@ -162,10 +165,16 @@ USE_TZ = True  # Enables timezone-aware datetime objects
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, "/static")
 # STATIC_URL = 'static/'
 # STATIC_ROOT = "/var/www/jomlah/static/"
 
@@ -174,7 +183,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = "/var/www/jomlah/media/"
-
+if not DEBUG:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
